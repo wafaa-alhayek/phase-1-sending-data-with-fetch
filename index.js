7f59c66ea7a8c5b49067074a65444aa9749abfda
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-27 18:37:01 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const configurationObj = {
+    method: "POST",
+    headers: {
+        "content-Type": "application/json",
+        Accept: "application/json",
+
+    },
+    body: JSON.stringify({
+        name: "Max",
+        email: "max@gmail.com"
+    }),
+
+};
+
+function submitData(name, email){
+    let body = document.querySelector('body')
+    return fetch("http://localhost:3000/users", {__proto__: configurationObj, body: JSON.stringify({
+        name: name, 
+        email: email,
+    })}).then(res =>res.json()).then(data => {
+        let p = document.createElement('p')
+        p.textContent = data.id
+        body.appendChild(p)
+    }).catch(error => {
+        let p =document.createElement('p')
+        p.textContent = error.message
+        body.appendChild(p)
+    })
+}
